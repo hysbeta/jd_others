b67c9ff70fec54a0246088d15a93f8c585bd7908
Fonghang <30502871+hysbeta@users.noreply.github.com>
2022-11-16 23:41:13 +0800

    Update fcwb.py

--- fcwb.py
+++ fcwb.py
@@ -6,7 +6,7 @@ new Env('发财挖宝');
 活动入口: 京东极速版 > 我的 > 发财挖宝
 最高可得总和为10元的微信零钱和红包
 脚本功能为: 挖宝，提现，没有助力功能，当血量剩余 1 时停止挖宝，领取奖励并提现 
-
+from : https://github.com/shufflewzc/faker3/edit/main/jd_fcwb.py
 目前需要完成逛一逛任务并且下单任务才能通关，不做的话大概可得1.5～2块的微信零钱
 '''
 import os,json,random,time,re,string,functools,asyncio
